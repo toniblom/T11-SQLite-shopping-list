e80df1c6e320e534e4b809c46d0ef8ea291ef75a
toniblom <toniblom91@gmail.com>
2023-10-11 18:02:23 +0300

    Tehtävän 15 täydennykset tehty

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
+import { StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 import * as SQLite from 'expo-sqlite';
+import { Header } from '@rneui/themed';
+import { Input } from '@rneui/themed';
+import { Button } from '@rneui/themed';
+import { ListItem } from '@rneui/themed';
+import { Icon } from '@rneui/themed';
 
 const db = SQLite.openDatabase('coursedb.db');
 
@@ -13,14 +18,14 @@ export default function App() {
   useEffect(() => {
     db.transaction(tx => {
       tx.executeSql('create table if not exists line (id integer primary key not null unique, product text, amount text);');
-    }, null, updateList); 
+    }, null, updateList);
   }, []);
 
   // Save item
   const saveItem = () => {
     db.transaction(tx => {
-        tx.executeSql('insert into line (product, amount) values (?, ?);', [product, amount]);    
-      }, null, updateList
+      tx.executeSql('insert into line (product, amount) values (?, ?);', [product, amount]);
+    }, null, updateList
     )
   }
 
@@ -29,7 +34,7 @@ export default function App() {
     db.transaction(tx => {
       tx.executeSql('select * from line;', [], (_, { rows }) =>
         setLines(rows._array)
-      ); 
+      );
     });
   }
 
@@ -39,54 +44,65 @@ export default function App() {
       tx => {
         tx.executeSql(`delete from line where id = ?;`, [id]);
       }, null, updateList
-    )    
+    )
   }
-  
-  const listSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 5,
-          width: "80%",
-          backgroundColor: "#fff",
-          marginLeft: "10%"
-        }}
-      />
-    );
-  };
+
+  // Apparently no need to have const in front here
+  renderItem = ({ item }) => (
+    <ListItem bottomDivider>
+      <View style={styles.listcontainer}>
+        <ListItem.Content>
+          <ListItem.Title>{item.product}</ListItem.Title>
+          <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
+        </ListItem.Content>
+        <Icon type="material" name="delete" color="red" onPress={() => deleteItem(item.id)} />
+      </View>
+    </ListItem>
+  );
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder='Product' style={{marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(amount) => setAmount(amount)}
-        value={amount}/>  
-      <TextInput placeholder='Amount' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(product) => setProduct(product)}
-        value={product}/>      
-      <Button onPress={saveItem} title="Save" /> 
-      <Text style={{marginTop: 30, fontSize: 20}}>Shopping list</Text>
-      <FlatList 
-        style={{marginLeft : "5%"}}
-        keyExtractor={item => item.id.toString()} 
-        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.amount}, {item.product}</Text>
-        <Text style={{fontSize: 18, color: '#0000ff'}} onPress={() => deleteItem(item.id)}> bought</Text></View>} 
-        data={lines} 
-        ItemSeparatorComponent={listSeparator} 
-      />      
+      <Header
+        centerComponent={{ text: 'SHOPPING LIST', style: { color: '#fff' } }}
+      />
+
+      <Input
+        placeholder='Product' label='PRODUCT'
+        onChangeText={product => setProduct(product)} value={product} />
+      <Input
+        placeholder='Amount' label='AMOUNT'
+        onChangeText={amount => setAmount(amount)} value={amount} />
+
+      <View style={styles.button}>
+        <Button raised icon={{ name: 'save', color: 'white' }} onPress={saveItem} title="SAVE" />
+      </View>
+
+      <FlatList
+        // Apparently both of these keyExtractors work:
+        //keyExtractor={item => item.id.toString()}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+        data={lines}
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
- },
- listcontainer: {
-  flexDirection: 'row',
-  backgroundColor: '#fff',
-  alignItems: 'center'
- },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'center', // This caused a problem for some (?) reason
+    justifyContent: 'center',
+  },
+  listcontainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center'
+  },
+  button: {
+    // Allows to change the button screen width
+    marginHorizontal: 80,
+  },
 });
